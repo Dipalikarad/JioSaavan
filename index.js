9487f160ad81b2a01d981bfb3d3de2ceada17f39
Dipali Karad <dipalikarad25@gmail.com>
2024-08-07 12:52:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,201 @@
+const audio=new Audio("Jhoome Jo Pathaan - Pathaan 320 Kbps.mp3")
+const audio1=new Audio("Kesariya - Brahmastra 128 Kbps.mp3")
+const audio2=new Audio("Show Me The Thumka - Tu Jhoothi Main Makkar 320 Kbps.mp3")
+const audio3=new Audio("Raabta (Title Track) - Raabta 320 Kbps.mp3")
+const audio4=new Audio("Manike - Thank God 320 Kbps.mp3")
+const audio5=new Audio("Heeriye (feat. Arijit Singh) - Dulquer Salmaan 320 Kbps.mp3")
+const audio6=new Audio("Besharam Rang - Pathaan 320 Kbps.mp3")
+const audio7=new Audio("Satranga - Animal 320 Kbps.mp3")
+const audio8=new Audio("Bekhayali - Kabir Singh 320 Kbps.mp3")
+const audio9=new Audio("O Maahi - Dunki 320 Kbps.mp3")
+const audio10=new Audio("Malang Sajna - Sachet Tandon 320 Kbps.mp3")
+const audio11=new Audio("Billi Billi - Fazilpuria 320 Kbps.mp3")
+const audio12=new Audio("Raataan Lambiyan - Shershaah 320 Kbps.mp3")
+const audio13=new Audio("Chaleya - Jawan 320 Kbps.mp3")
+const audio14=new Audio("Aayi Nai - Stree 2 320 Kbps.mp3")
+const audio15=new Audio("Haste Haste - Phir Aayi Hasseen Dillruba 320 Kbps.mp3")
+const audio16=new Audio("Aaj Ki Raat - Stree 2 320 Kbps.mp3")
+const audio17=new Audio("Punjabi Munde - Ghudchadi 320 Kbps.mp3")
+const audio18=new Audio("Fire - Baali Cheema 320 Kbps.mp3")
+const audio19=new Audio("The Escape - Kalki 2898 Ad 320 Kbps.mp3")
+const audio20=new Audio("Kill (Kaawaa Kaawaa) - Kill 320 Kbps.mp3")
+const audio21=new Audio("Duur Na Karin - Khel Khel Mein 320 Kbps.mp3")
+const audio22=new Audio("Shaukan - Ulajh 320 Kbps.mp3")
+const audio23=new Audio("Hauli Hauli - De De Pyaar De 320 Kbps.mp3")
+const audio24=new Audio("Tauba Tauba - Bad Newz 320 Kbps.mp3")
+const audio25=new Audio("Khudaya - Sarfira 320 Kbps.mp3")
+const audio26=new Audio("Husn Irani - Wild Wild Punjab 320 Kbps.mp3")
+const audio27=new Audio("Maayane - Surya's Saturday (Hindi) (pagalall.com).mp3")
+
+
+
+
+document.querySelector(".audio").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".btn").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn1").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio1").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn2").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio2").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn3").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio3").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn4").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio4").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn5").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio5").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn6").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio6").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn7").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio7").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn8").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio8").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn9").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio9").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn10").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio10").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn11").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio11").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn12").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio12").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn13").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio13").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn14").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio14").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn15").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio15").addEventListener("click",function(){
+    audio.play();
+})
+
+document.querySelector(".btn16").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio16").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn17").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio17").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn18").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio18").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn19").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio19").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn20").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio20").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn21").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio21").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn22").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio22").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn23").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio23").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn24").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio24").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn25").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio25").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn26").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio26").addEventListener("click",function(){
+    audio.play();
+})
+document.querySelector(".btn27").addEventListener("click",function(){
+    audio.pause();
+})
+document.querySelector(".audio27").addEventListener("click",function(){
+    audio.play();
+})
